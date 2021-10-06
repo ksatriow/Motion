e383d15f065466741f5a9877c6105b12cdac4af4
ksatriow <andkukuh@gmail.com>
2021-10-06 11:05:01 +0700

    add search adapter

---
+++ app/src/main/java/com/satrio/motion/ui/adapter/SearchAdapter.kt
@@ -0,0 +1,69 @@
+package com.satrio.motion.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.satrio.motion.R
+import com.satrio.motion.data.entity.Movie
+import com.satrio.motion.databinding.ItemSearchBinding
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.satrio.motion.util.AppConstant
+
+@ExperimentalCoroutinesApi
+class SearchAdapter() :
+    PagingDataAdapter<Movie, SearchAdapter.ViewHolder>(MOVIE_COMPARATOR) {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding = ItemSearchBinding.bind(itemView)
+
+        fun bind(movie: Movie) {
+            binding.apply {
+                searchImage.load(AppConstant.IMGBASEURL + movie.poster_path) {
+                    placeholder(AppConstant.moviePlaceHolder[position % 4])
+                    error(AppConstant.moviePlaceHolder[position % 4])
+                }
+                itemView.setOnClickListener {
+                    val bundle = bundleOf(AppConstant.movie to movie)
+                    it.findNavController()
+                        .navigate(R.id.action_searchFragment_to_movieDetailsFragment, bundle)
+                }
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_search, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val currentItem = getItem(position)
+        if (currentItem != null) {
+            holder.bind(currentItem)
+        }
+
+    }
+
+
+    companion object {
+        private val MOVIE_COMPARATOR = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean =
+                oldItem == newItem
+
+        }
+    }
+
+
+}
