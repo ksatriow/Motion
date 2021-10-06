1ed936c3401b0f39f2da1f193587190eb814d995
ksatriow <andkukuh@gmail.com>
2021-10-06 11:00:53 +0700

    add extension fragment util

---
+++ app/src/main/java/com/satrio/motion/util/FragmentUtils.kt
@@ -0,0 +1,12 @@
+package com.satrio.motion.util
+
+import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+fun Fragment.showToast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(requireContext(), message, duration).show()
+}
+
+fun Fragment.getColorRes(@ColorRes id: Int) = ContextCompat.getColor(requireContext(), id)
